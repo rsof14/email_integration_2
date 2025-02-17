1d1ddad985e3e27b18201934865f1beff786a878
rsof14 <sofia.rytik@gmail.com>
2025-02-18 00:39:48 +0300

    ws: established websocket connection

--- src/api/emails.py
+++ src/api/emails.py
@@ -1,11 +1,26 @@
+import asyncio
+from typing import Dict
 from fastapi import APIRouter, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import RedirectResponse
-from starsessions import load_session
+from fastapi.websockets import WebSocket, WebSocketDisconnect
 
 
 router = APIRouter()
 templates = Jinja2Templates(directory="templates")
+active_connections: Dict[str, WebSocket] = {}
+
+@router.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    print(f'ws {websocket.session}')
+    active_connections[websocket.session['email']] = websocket
+    try:
+        while True:
+            await websocket.send_json({'msg': 'test connection'})
+            await asyncio.sleep(5)
+    except WebSocketDisconnect:
+        active_connections.pop(websocket.session['email'], None)
 
 
 @router.get('/')
