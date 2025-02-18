7ea338d2046090beefdf22949e8028d01229ac24
rsof14 <sofia.rytik@gmail.com>
2025-02-18 11:01:28 +0300

    ws: added dict with active connections

--- src/api/emails.py
+++ src/api/emails.py
@@ -2,19 +2,26 @@ import asyncio
 from fastapi import APIRouter, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import RedirectResponse
-from fastapi.websockets import WebSocket
+from fastapi.websockets import WebSocket, WebSocketDisconnect
+from typing import Dict
 
 
 router = APIRouter()
 templates = Jinja2Templates(directory="templates")
+active_connections: Dict[str, WebSocket] = {}
 
 
 @router.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
-    while True:
-        await websocket.send_json({'msg': f'Connected {websocket.session['email']}'})
-        await asyncio.sleep(5)
+    user_email = websocket.session['email']
+    active_connections[user_email] = websocket
+    try:
+        while True:
+            await websocket.send_json({'msg': f'Connected {user_email}'})
+            await asyncio.sleep(5)
+    except WebSocketDisconnect:
+        active_connections.pop(user_email, None)
 
 
 @router.get('/')
